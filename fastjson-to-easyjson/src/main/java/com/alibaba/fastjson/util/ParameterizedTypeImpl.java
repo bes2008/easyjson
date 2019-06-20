8d56b51d9072fdcb682be63371a890ead1e434a3
fs1194361820@163.com <fs1194361820@163.com>
2019-06-20 08:24:35 +0800

    * supports fastjson to easyjson

---
+++ fastjson-to-easyjson/src/main/java/com/alibaba/fastjson/util/ParameterizedTypeImpl.java
@@ -0,0 +1,68 @@
+/*
+ *  Copyright 2019 the original author or authors.
+ *
+ *  Licensed under the LGPL, Version 3.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at  http://www.gnu.org/licenses/lgpl-3.0.html
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
+package com.alibaba.fastjson.util;
+
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.Arrays;
+
+public class ParameterizedTypeImpl implements ParameterizedType {
+
+    private final Type[] actualTypeArguments;
+    private final Type   ownerType;
+    private final Type   rawType;
+
+    public ParameterizedTypeImpl(Type[] actualTypeArguments, Type ownerType, Type rawType){
+        this.actualTypeArguments = actualTypeArguments;
+        this.ownerType = ownerType;
+        this.rawType = rawType;
+    }
+
+    public Type[] getActualTypeArguments() {
+        return actualTypeArguments;
+    }
+
+    public Type getOwnerType() {
+        return ownerType;
+    }
+
+    public Type getRawType() {
+        return rawType;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        ParameterizedTypeImpl that = (ParameterizedTypeImpl) o;
+
+        // Probably incorrect - comparing Object[] arrays with Arrays.equals
+        if (!Arrays.equals(actualTypeArguments, that.actualTypeArguments)) return false;
+        if (ownerType != null ? !ownerType.equals(that.ownerType) : that.ownerType != null) return false;
+        return rawType != null ? rawType.equals(that.rawType) : that.rawType == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = actualTypeArguments != null ? Arrays.hashCode(actualTypeArguments) : 0;
+        result = 31 * result + (ownerType != null ? ownerType.hashCode() : 0);
+        result = 31 * result + (rawType != null ? rawType.hashCode() : 0);
+        return result;
+    }
+}
