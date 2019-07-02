5627a3bfbf5b86b7e260c95a757133a105c9c4c6
fjn <fs1194361820@163.com>
2019-07-02 18:52:55 +0800

    * set copyright

--- jsonsmart-to-easyjson/src/main/java/net/minidev/json/JSONNavi.java
+++ jsonsmart-to-easyjson/src/main/java/net/minidev/json/JSONNavi.java
@@ -1,13 +1,9 @@
-package net.minidev.json;
-
 /*
- *    Copyright 2011 JSON-SMART authors
+ * Copyright 2019 the original author or authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the LGPL, Version 3.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
+ * You may obtain a copy of the License at  http://www.gnu.org/licenses/lgpl-3.0.html
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -15,6 +11,8 @@ package net.minidev.json;
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+package net.minidev.json;
 
 import net.minidev.json.writer.FakeMapper;
 import net.minidev.json.writer.JsonReaderI;
