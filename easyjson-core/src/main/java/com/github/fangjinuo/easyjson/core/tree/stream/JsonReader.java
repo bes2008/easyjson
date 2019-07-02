b8c637f32ddeb93072ea7f09bb24b4696eb8dd2b
fjn <fs1194361820@163.com>
2019-07-02 14:40:57 +0800

    * add @Override

--- easyjson-core/src/main/java/com/github/fangjinuo/easyjson/core/tree/stream/JsonReader.java
+++ easyjson-core/src/main/java/com/github/fangjinuo/easyjson/core/tree/stream/JsonReader.java
@@ -1223,6 +1223,7 @@ public class JsonReader implements Closeable {
     /**
      * Closes this JSON reader and the underlying {@link java.io.Reader}.
      */
+    @Override
     public void close() throws IOException {
         peeked = PEEKED_NONE;
         stack[0] = JsonScope.CLOSED;
