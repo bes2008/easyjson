904b19caccb40cfa15c1a36a8271cf5389e9b8e2
fjn <fs1194361820@163.com>
2019-08-01 14:21:40 +0800

    + add easyjson adapter

---
+++ moshi-to-easyjson/src/main/java/com/squareup/moshi/easyjson/EasyJsonAdapter.java
@@ -0,0 +1,118 @@
+/*
+ * Copyright 2019 the original author or authors.
+ *
+ * Licensed under the LGPL, Version 3.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at  http://www.gnu.org/licenses/lgpl-3.0.html
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.squareup.moshi.easyjson;
+
+import com.jn.easyjson.core.JSONBuilder;
+import com.squareup.moshi.JsonAdapter;
+import com.squareup.moshi.JsonReader;
+import com.squareup.moshi.JsonWriter;
+import okio.Buffer;
+
+import javax.annotation.Nullable;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.lang.reflect.Type;
+
+public class EasyJsonAdapter<E> extends JsonAdapter<E> {
+    private JSONBuilder jsonBuilder;
+    private Type type;
+    private JsonAdapter delegate;
+
+    @Nullable
+    @Override
+    public E fromJson(JsonReader reader) throws IOException {
+        String jsonString = reader.getJsonString();
+        return jsonBuilder.build().fromJson(jsonString, type);
+    }
+
+    @Override
+    public void toJson(JsonWriter writer, @Nullable E value) throws IOException {
+        String jsonString = jsonBuilder.build().toJson(value);
+        Buffer buffer = new Buffer().readFrom(new ByteArrayInputStream(jsonString.getBytes()));
+        writer.value(buffer);
+    }
+
+
+    public Type getType() {
+        return type;
+    }
+
+    public void setType(Type type) {
+        this.type = type;
+    }
+
+    public JsonAdapter getDelegate() {
+        return delegate;
+    }
+
+    public void setDelegate(JsonAdapter delegate) {
+        this.delegate = delegate;
+    }
+
+    public JSONBuilder getJsonBuilder() {
+        return jsonBuilder;
+    }
+
+    public void setJsonBuilder(JSONBuilder jsonBuilder) {
+        this.jsonBuilder = jsonBuilder;
+    }
+
+    @Override
+    public JsonAdapter<E> lenient() {
+        JSONBuilder newBuilder = JSONBuilder.clone(jsonBuilder);
+        newBuilder.setLenient(true);
+
+        return wrap(this, newBuilder);
+    }
+
+    @Override
+    public JsonAdapter<E> serializeNulls() {
+        return serializeNulls(true);
+    }
+
+    private JsonAdapter<E> serializeNulls(boolean serializeNulls) {
+        JSONBuilder newBuilder = JSONBuilder.clone(jsonBuilder);
+        newBuilder.serializeNulls(serializeNulls);
+        return wrap(this, newBuilder);
+    }
+
+    @Override
+    public JsonAdapter<E> nullSafe() {
+        return serializeNulls();
+    }
+
+    @Override
+    public JsonAdapter<E> nonNull() {
+        return serializeNulls(false);
+    }
+
+    @Override
+    public JsonAdapter<E> failOnUnknown() {
+        return wrap(this, JSONBuilder.clone(jsonBuilder));
+    }
+
+    private static EasyJsonAdapter wrap(EasyJsonAdapter jsonAdapter, JSONBuilder jsonBuilder) {
+        EasyJsonAdapter wrapper = new EasyJsonAdapter();
+        wrapper.delegate = jsonAdapter;
+        wrapper.type = jsonAdapter.type;
+        wrapper.setJsonBuilder(jsonBuilder);
+        return wrapper;
+    }
+
+    public boolean isLenient() {
+        return jsonBuilder.isLenient();
+    }
+
+}
---
+++ moshi-to-easyjson/src/main/java/com/squareup/moshi/easyjson/EasyJsonAdapterFactory.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2019 the original author or authors.
+ *
+ * Licensed under the LGPL, Version 3.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at  http://www.gnu.org/licenses/lgpl-3.0.html
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.squareup.moshi.easyjson;
+
+import com.jn.easyjson.core.JSONBuilder;
+import com.jn.easyjson.core.JSONBuilderProvider;
+import com.squareup.moshi.JsonAdapter;
+import com.squareup.moshi.Moshi;
+
+import javax.annotation.Nullable;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Type;
+import java.util.Set;
+
+public class EasyJsonAdapterFactory implements JsonAdapter.Factory {
+
+    private JsonAdapter.Factory delegate;
+
+    public EasyJsonAdapterFactory(JsonAdapter.Factory delegate) {
+        this.delegate = delegate;
+    }
+
+    @Nullable
+    @Override
+    public JsonAdapter<?> create(Type type, Set<? extends Annotation> annotations, Moshi moshi) {
+        JsonAdapter adapterDelegate = delegate.create(type, annotations, moshi);
+        JSONBuilder jsonBuilder = JSONBuilderProvider.create();
+        EasyJsonAdapter adapter = new EasyJsonAdapter();
+        adapter.setJsonBuilder(jsonBuilder);
+        adapter.setDelegate(adapterDelegate);
+        adapter.setType(type);
+        return adapter;
+    }
+}
