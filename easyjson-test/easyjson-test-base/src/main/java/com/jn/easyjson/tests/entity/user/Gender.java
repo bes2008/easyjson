6a3c254847cdd8d9a67d9efaba0f0c076df4f512
zhaohuihua <zhaohuihua@126.com>
2020-06-14 11:31:40 +0800

    用户实体测试类

---
+++ easyjson-test/easyjson-test-base/src/main/java/com/jn/easyjson/tests/entity/user/Gender.java
@@ -0,0 +1,21 @@
+package com.jn.easyjson.tests.entity.user;
+
+/**
+ * Gender枚举类<br>
+ * 性别:0.未知|1.男|2.女
+ *
+ * @author zhh
+ * @version 150917
+ */
+public enum Gender {
+    
+    /** 0.未知 **/
+    UNKNOWN,
+    
+    /** 1.男 **/
+    MALE,
+    
+    /** 2.女 **/
+    FEMALE;
+    
+}
---
+++ easyjson-test/easyjson-test-base/src/main/java/com/jn/easyjson/tests/entity/user/UserEntity.java
@@ -0,0 +1,111 @@
+package com.jn.easyjson.tests.entity.user;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class UserEntity implements Serializable {
+
+    /** serialVersionUID **/
+    private static final long serialVersionUID = 1L;
+
+    private String id;
+    private String name;
+    private Gender gender;
+    private String password;
+    private String email;
+    private double weight;
+    private int height;
+    private Date birthday;
+    private String intro;
+    private List<Address> addresses;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getIntro() {
+        return intro;
+    }
+
+    public void setIntro(String intro) {
+        this.intro = intro;
+    }
+
+    public List<Address> getAddresses() {
+        return addresses;
+    }
+
+    public void setAddresses(List<Address> addresses) {
+        this.addresses = addresses;
+    }
+
+    public void addAddress(Address address) {
+        if (this.addresses == null) {
+            this.addresses = new ArrayList<>();
+        }
+        this.addresses.add(address);
+    }
+
+}
