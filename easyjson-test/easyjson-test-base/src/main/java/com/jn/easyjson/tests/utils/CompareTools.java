ea47f953da61c2bc6009b093026769cbccdae080
zhaohuihua <zhaohuihua@126.com>
2020-06-14 13:57:18 +0800

    CompareTools

--- easyjson-test/easyjson-test-base/src/main/java/com/jn/easyjson/tests/utils/CompareTools.java
+++ easyjson-test/easyjson-test-base/src/main/java/com/jn/easyjson/tests/utils/CompareTools.java
@@ -19,11 +19,11 @@ import com.jn.langx.util.reflect.Reflects;
 public class CompareTools {
 
     /** 比对两个对象的字段值是否一致 **/
-    public static void assertFieldValueEquals(Object actual, Object expected) {
-        assertFieldValueEquals(null, actual, expected);
+    public static void assertDeepEquals(Object actual, Object expected) {
+        assertDeepEquals(null, actual, expected);
     }
 
-    private static void assertFieldValueEquals(String owner, Object actual, Object expected) {
+    private static void assertDeepEquals(String owner, Object actual, Object expected) {
         String desc = (owner == null ? "" : owner);
         // 都为空
         if (actual == null && expected == null) {
@@ -41,64 +41,68 @@ public class CompareTools {
         } else if (clazz.isArray()) { // 数组类
             List<Object> aList = Arrays.asList((Object[]) actual);
             List<Object> eList = Arrays.asList((Object[]) expected);
-            assertListValueEquals(owner, aList, eList);
+            assertListValueEquals(owner == null ? clazz.getSimpleName() : owner, aList, eList);
         } else if (Collection.class.isAssignableFrom(clazz)) { // 集合类
             List<Object> aList = new ArrayList<>();
             aList.addAll((Collection<?>) actual);
             List<Object> eList = new ArrayList<>();
             eList.addAll((Collection<?>) expected);
-            assertListValueEquals(owner, aList, eList);
+            assertListValueEquals(owner == null ? clazz.getSimpleName() : owner, aList, eList);
         } else if (Map.class.isAssignableFrom(clazz)) { // MAP类
             // TODO
-        } else { // 其他类比较字段
-            // 遍历所有字段
-            Collection<Field> fields = Reflects.getAllDeclaredFields(clazz);
-            for (Field field : fields) {
-                field.setAccessible(true);
-                // 获取actual的字段值
-                Object aValue = null;
-                Exception aException = null;
-                try {
-                    aValue = field.get(actual);
-                } catch (Exception e) {
-                    aException = e;
-                }
-                // 获取expected的字段值
-                Object eValue = null;
-                Exception eException = null;
-                try {
-                    eValue = field.get(expected);
-                } catch (Exception e) {
-                    eException = e;
-                }
-                desc = (owner == null ? clazz.getSimpleName() : owner) + "." + field.getName();
-                // 如果发生异常了, 异常必须相等, 否则字段值必须相等
-                if (aException != null || eException != null) {
-                    System.out.println("actual   " + desc + " = " + aException.toString());
-                    System.out.println("expected " + desc + " = " + eException.toString());
-                    Assert.assertEquals(aException.getClass(), eException.getClass(), "Get " + desc + " exception");
-                    Assert.assertEquals(aException.getMessage(), eException.getMessage(), "Get " + desc + " exception");
-                    continue;
-                }
-                assertFieldValueEquals(desc, aValue, eValue);
-            }
+        } else { // 其他类型, 比较字段值
+            assertFieldValueEquals(owner == null ? clazz.getSimpleName() : owner, actual, expected);
         }
     }
 
     private static void assertListValueEquals(String owner, List<?> aList, List<?> eList) {
-        String desc = owner == null ? "" : owner;
         // 空集合
         if (aList.isEmpty() && eList.isEmpty()) {
-            System.out.println("actual   " + desc + " = " + "[]");
-            System.out.println("expected " + desc + " = " + "[]");
+            System.out.println("actual   " + owner + " = " + "[]");
+            System.out.println("expected " + owner + " = " + "[]");
             return;
         }
-        Assert.assertEquals(aList.size(), eList.size(), (owner == null ? "" : owner + " ") + "size");
+        Assert.assertEquals(aList.size(), eList.size(), owner + " size");
 
         for (int i = 0; i < aList.size(); i++) {
             Object aItem = aList.get(i);
             Object eItem = eList.get(i);
-            assertFieldValueEquals(desc + '[' + i + ']', aItem, eItem);
+            assertDeepEquals(owner + '[' + i + ']', aItem, eItem);
+        }
+    }
+    
+    private static void assertFieldValueEquals(String owner, Object actual, Object expected) {
+        // 遍历所有字段
+        Class<?> clazz = actual.getClass();
+        Collection<Field> fields = Reflects.getAllDeclaredFields(clazz);
+        for (Field field : fields) {
+            field.setAccessible(true);
+            // 获取actual的字段值
+            Object aValue = null;
+            Exception aException = null;
+            try {
+                aValue = field.get(actual);
+            } catch (Exception e) {
+                aException = e;
+            }
+            // 获取expected的字段值
+            Object eValue = null;
+            Exception eException = null;
+            try {
+                eValue = field.get(expected);
+            } catch (Exception e) {
+                eException = e;
+            }
+            String desc = owner + "." + field.getName();
+            // 如果发生异常了, 异常必须相等, 否则字段值必须相等
+            if (aException != null || eException != null) {
+                System.out.println("actual   " + desc + " = " + aException.toString());
+                System.out.println("expected " + desc + " = " + eException.toString());
+                Assert.assertEquals(aException.getClass(), eException.getClass(), "Get " + desc + " exception");
+                Assert.assertEquals(aException.getMessage(), eException.getMessage(), "Get " + desc + " exception");
+                continue;
+            }
+            assertDeepEquals(desc, aValue, eValue);
         }
     }
 
