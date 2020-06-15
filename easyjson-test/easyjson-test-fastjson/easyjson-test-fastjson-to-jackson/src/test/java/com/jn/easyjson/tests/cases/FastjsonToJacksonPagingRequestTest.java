fd649f37c35c37dfa5a9421dba2a2eeb03611e34
zhaohuihua <zhaohuihua@126.com>
2020-06-15 08:09:32 +0800

    EasyJsonPagingRequestTest

---
+++ easyjson-test/easyjson-test-fastjson/easyjson-test-fastjson-to-jackson/src/test/java/com/jn/easyjson/tests/cases/FastjsonToJacksonPagingRequestTest.java
@@ -0,0 +1,11 @@
+package com.jn.easyjson.tests.cases;
+
+/**
+ * FastjsonToJackson复杂对象测试
+ *
+ * @author zhaohuihua
+ * @version 20200615
+ */
+public class FastjsonToJacksonPagingRequestTest extends EasyJsonPagingRequestTest {
+
+}
