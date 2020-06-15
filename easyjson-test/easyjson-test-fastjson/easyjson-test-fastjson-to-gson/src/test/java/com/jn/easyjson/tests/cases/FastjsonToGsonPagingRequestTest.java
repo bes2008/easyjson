fe9016bf86d7492a1a17c62d4cbbe36061b88da5
zhaohuihua <zhaohuihua@126.com>
2020-06-15 08:10:21 +0800

    FastjsonToGsonPagingRequestTest

---
+++ easyjson-test/easyjson-test-fastjson/easyjson-test-fastjson-to-gson/src/test/java/com/jn/easyjson/tests/cases/FastjsonToGsonPagingRequestTest.java
@@ -0,0 +1,11 @@
+package com.jn.easyjson.tests.cases;
+
+/**
+ * FastjsonToGson复杂对象测试
+ *
+ * @author zhaohuihua
+ * @version 20200615
+ */
+public class FastjsonToGsonPagingRequestTest extends EasyJsonPagingRequestTest {
+
+}
