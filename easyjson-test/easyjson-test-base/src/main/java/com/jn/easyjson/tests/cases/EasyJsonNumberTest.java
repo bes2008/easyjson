85f77bd5c4945287abc25760e6e5031c9ac8e17d
zhaohuihua <zhaohuihua@126.com>
2020-06-19 08:17:49 +0800

    修改数字测试用例

--- easyjson-test/easyjson-test-base/src/main/java/com/jn/easyjson/tests/cases/EasyJsonNumberTest.java
+++ easyjson-test/easyjson-test-base/src/main/java/com/jn/easyjson/tests/cases/EasyJsonNumberTest.java
@@ -5,14 +5,57 @@ import java.util.HashMap;
 import java.util.Map;
 import org.testng.annotations.Test;
 import com.jn.easyjson.core.JSON;
-import com.jn.easyjson.core.JSONBuilder;
 import com.jn.easyjson.core.JSONBuilderProvider;
-import com.jn.easyjson.tests.utils.CompareTools;
+import com.jn.easyjson.tests.utils.Asserts;
 import com.jn.langx.util.reflect.type.Types;
 
 public abstract class EasyJsonNumberTest extends AbstractBaseTest {
 
     private static JSON json = JSONBuilderProvider.simplest();
+
+    @Test(priority = 10001)
+    public void testSerializeIntegralNumber10001() {
+        Asserts.assertEquals(json.toJson(0), "0");
+        Asserts.assertEquals(json.toJson(1), "1");
+        Asserts.assertEquals(json.toJson(-1), "-1");
+        Asserts.assertEquals(json.toJson(999999999), "999999999");
+        Asserts.assertEquals(json.toJson(-999999999), "-999999999");
+        Asserts.assertEquals(json.toJson(Integer.MAX_VALUE), String.valueOf(Integer.MAX_VALUE));
+        Asserts.assertEquals(json.toJson(Integer.MIN_VALUE), String.valueOf(Integer.MIN_VALUE));
+    }
+
+    @Test(priority = 10002)
+    public void testDeserializeIntegralNumber10002() {
+        Asserts.assertEquals(json.fromJson("0", int.class), 0);
+        Asserts.assertEquals(json.fromJson("1", int.class), 1);
+        Asserts.assertEquals(json.fromJson("-1", int.class), -1);
+        Asserts.assertEquals(json.fromJson("999999999", int.class), 999999999);
+        Asserts.assertEquals(json.fromJson("-999999999", int.class), -999999999);
+        Asserts.assertEquals(json.fromJson(String.valueOf(Integer.MAX_VALUE), int.class), Integer.MAX_VALUE);
+        Asserts.assertEquals(json.fromJson(String.valueOf(Integer.MIN_VALUE), int.class), Integer.MIN_VALUE);
+    }
+
+    @Test(priority = 10003)
+    public void testSerializeDoubleNumber10003() {
+        Asserts.assertEquals(json.toJson(0.0), "0.0");
+        Asserts.assertEquals(json.toJson(1.0), "1.0");
+        Asserts.assertEquals(json.toJson(-1.0), "-1.0");
+        Asserts.assertEquals(json.toJson(999999999.0), "999999999.0");
+        Asserts.assertEquals(json.toJson(-999999999.0), "-999999999.0");
+        Asserts.assertEquals(json.toJson(Double.MAX_VALUE), String.valueOf(Double.MAX_VALUE));
+        Asserts.assertEquals(json.toJson(Double.MIN_VALUE), String.valueOf(Double.MIN_VALUE));
+    }
+
+    @Test(priority = 10004)
+    public void testDeserializeDoubleNumber10004() {
+        Asserts.assertEquals(json.fromJson("0.0", double.class), 0.0);
+        Asserts.assertEquals(json.fromJson("1.0", double.class), 1.0);
+        Asserts.assertEquals(json.fromJson("-1.0", double.class), -1.0);
+        Asserts.assertEquals(json.fromJson("999999999.0", double.class), 999999999.0);
+        Asserts.assertEquals(json.fromJson("-999999999.0", double.class), -999999999.0);
+        Asserts.assertEquals(json.fromJson(String.valueOf(Double.MAX_VALUE), double.class), Double.MAX_VALUE);
+        Asserts.assertEquals(json.fromJson(String.valueOf(Double.MIN_VALUE), double.class), Double.MIN_VALUE);
+    }
 
     protected String getStringNumberMap1String() {
         return readClassResourceText(EasyJsonBaseTest.class, "json/NumberWithStringValue.json");
@@ -22,54 +65,17 @@ public abstract class EasyJsonNumberTest extends AbstractBaseTest {
         return readClassResourceText(EasyJsonBaseTest.class, "json/NumberWithOriginalValue.json");
     }
 
-    protected Map<String, Integer> getIntegerMap1Object() {
-        Map<String, Integer> map = new HashMap<>();
-        map.put("number1", 0);
-        map.put("number2", 1);
-        map.put("number3", 128);
-        map.put("number4", 10000);
-        map.put("number5", 10000);
-        map.put("number6", (int) 1423232322323534L);
-        map.put("number7", 2);
-        map.put("number8", 1000000000);
-        return map;
-    }
-
     protected Map<String, Double> getDoubleMap2Object() {
         Map<String, Double> map = new HashMap<>();
-        map.put("number1", 0D);
+        map.put("number1", 0.0);
         map.put("number2", 1.0);
         map.put("number3", 128.0);
         map.put("number4", 10000.0);
         map.put("number5", 10000.0000003);
-        map.put("number6", 1423232322323534.0);
-        map.put("number7", 1.9999);
+        map.put("number6", 1.9999);
+        map.put("number7", 1423232322323534.0);
         map.put("number8", 999999999.999999999);
         return map;
-    }
-
-    @Test(priority = 10001)
-    public void testSerializeOritinalNumber10001() {
-        JSONBuilder jsonBuilder = JSONBuilderProvider.create();
-        jsonBuilder.serializeNumberAsString(false);
-        JSON json = jsonBuilder.build();
-        Map<String, Double> map = getDoubleMap2Object();
-        String actual = json.toJson(map);
-        System.out.println(actual);
-        String expected = getOritinalNumberMap2String();
-        CompareTools.assertJsonEquals(actual, expected);
-    }
-
-    @Test(priority = 10002)
-    public void testSerializeOritinalNumberAsString10002() {
-        JSONBuilder jsonBuilder = JSONBuilderProvider.create();
-        jsonBuilder.serializeNumberAsString(true);
-        JSON json = jsonBuilder.build();
-        Map<String, Double> map = getDoubleMap2Object();
-        String actual = json.toJson(map);
-        System.out.println(actual);
-        String expected = getStringNumberMap1String();
-        CompareTools.assertJsonEquals(actual, expected);
     }
 
     @Test(priority = 10101)
@@ -78,7 +84,7 @@ public abstract class EasyJsonNumberTest extends AbstractBaseTest {
         ParameterizedType type = Types.getMapParameterizedType(String.class, Double.class);
         Map<String, Double> actual = json.fromJson(jsonString, type);
         Map<String, Double> expected = getDoubleMap2Object();
-        CompareTools.assertDeepEquals(actual, expected);
+        Asserts.assertDeepEquals(actual, expected);
     }
 
     @Test(priority = 10102)
@@ -87,24 +93,7 @@ public abstract class EasyJsonNumberTest extends AbstractBaseTest {
         ParameterizedType type = Types.getMapParameterizedType(String.class, Double.class);
         Map<String, Double> actual = json.fromJson(jsonString, type);
         Map<String, Double> expected = getDoubleMap2Object();
-        CompareTools.assertDeepEquals(actual, expected);
+        Asserts.assertDeepEquals(actual, expected);
     }
 
-    @Test(priority = 10103)
-    public void testDeserializeStringNumberToInteger10103() {
-        String jsonString = getStringNumberMap1String();
-        ParameterizedType type = Types.getMapParameterizedType(String.class, Integer.class);
-        Map<String, Integer> actual = json.fromJson(jsonString, type);
-        Map<String, Integer> expected = getIntegerMap1Object();
-        CompareTools.assertDeepEquals(actual, expected);
-    }
-
-    @Test(priority = 10104)
-    public void testDeserializeOritinalNumberToInteger10104() {
-        String jsonString = getOritinalNumberMap2String();
-        ParameterizedType type = Types.getMapParameterizedType(String.class, Integer.class);
-        Map<String, Integer> actual = json.fromJson(jsonString, type);
-        Map<String, Integer> expected = getIntegerMap1Object();
-        CompareTools.assertDeepEquals(actual, expected);
-    }
 }
