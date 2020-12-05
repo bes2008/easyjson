bde8cfe306066d4955befdd205622e4c2f646fad
fjn <1194361820@qq.com>
2020-12-05 15:20:02 +0800

    * 添加 gson 适配到 jackson 的测试用例

---
+++ easyjson-test/easyjson-test-gson/easyjson-test-gson-to-jackson/src/test/java/com/jn/easyjson/test/gson/jackson/cases/GsonImplAnnotationTest.java
@@ -0,0 +1,14 @@
+package com.jn.easyjson.test.gson.jackson.cases;
+
+import com.jn.easyjson.tests.cases.GsonAnnotationTest;
+import org.testng.annotations.Test;
+
+/**
+ * Gson注解测试类
+ *
+ * @author zhaohuihua
+ * @version 20200614
+ */
+@Test
+public class GsonImplAnnotationTest extends GsonAnnotationTest {
+}
---
+++ easyjson-test/easyjson-test-gson/easyjson-test-gson-to-jackson/src/test/java/com/jn/easyjson/test/gson/jackson/cases/GsonImplBaseTest.java
@@ -0,0 +1,14 @@
+package com.jn.easyjson.test.gson.jackson.cases;
+
+import com.jn.easyjson.tests.cases.EasyJsonBaseTest;
+import org.testng.annotations.Test;
+
+/**
+ * GsonToGsonBaseTest基础测试
+ *
+ * @author zhaohuihua
+ * @version 20200614
+ */
+@Test
+public class GsonImplBaseTest extends EasyJsonBaseTest {
+}
