f8f2507641f9f9c0dc2c736b2e50c5edc853e071
fjn <1194361820@qq.com>
2020-12-05 16:00:42 +0800

    * 更新License说明

--- fastjson-to-easyjson/src/main/java/com/alibaba/fastjson/annotation/JSONField.java
+++ fastjson-to-easyjson/src/main/java/com/alibaba/fastjson/annotation/JSONField.java
@@ -1,7 +1,7 @@
 /*
  *  Copyright 2019 the original author or authors.
  *
- *  Licensed under the LGPL, Version 3.0 (the "License");
+ *  Licensed under the Apache, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
  *  You may obtain a copy of the License at  http://www.gnu.org/licenses/lgpl-3.0.html
  *
