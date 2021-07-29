2c7b2d770c0c13af1ed1389b2d875abb4e4a6f26
fjn <1194361820@qq.com>
2021-07-29 11:57:35 +0800

    * 为 GsonExcluderExclusion 支持 equals hashcode

--- gson-to-easyjson/src/main/java/com/google/gson/easyjson/GsonExcluderExclusion.java
+++ gson-to-easyjson/src/main/java/com/google/gson/easyjson/GsonExcluderExclusion.java
@@ -2,13 +2,15 @@ package com.google.gson.easyjson;
 
 import com.google.gson.internal.Excluder;
 import com.jn.easyjson.core.exclusion.Exclusion;
+import com.jn.langx.util.Objs;
 import com.jn.langx.util.reflect.FieldAttributes;
 import com.jn.langx.util.reflect.MethodAttributes;
+
 
 public class GsonExcluderExclusion implements Exclusion {
     private Excluder excluder;
 
-    public GsonExcluderExclusion(Excluder excluder){
+    public GsonExcluderExclusion(Excluder excluder) {
         this.excluder = excluder;
     }
 
@@ -25,5 +27,22 @@ public class GsonExcluderExclusion implements Exclusion {
     @Override
     public boolean shouldSkipClass(Class<?> clazz, boolean serializePhrase) {
         return this.excluder.excludeClass(clazz, serializePhrase);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+
+        GsonExcluderExclusion that = (GsonExcluderExclusion) o;
+        return Objs.equals(excluder, that.excluder);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objs.hash(excluder);
     }
 }
