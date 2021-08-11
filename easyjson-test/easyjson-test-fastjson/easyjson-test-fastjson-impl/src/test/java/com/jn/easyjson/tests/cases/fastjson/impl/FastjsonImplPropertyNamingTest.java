81119c20a1fea8924428a70fcad0e0ffeb9e4a55
fjn <1194361820@qq.com>
2021-08-11 19:00:44 +0800

    * 测试fastjson 对命名的支持

---
+++ easyjson-test/easyjson-test-fastjson/easyjson-test-fastjson-impl/src/test/java/com/jn/easyjson/tests/cases/fastjson/impl/FastjsonImplPropertyNamingTest.java
@@ -0,0 +1,7 @@
+package com.jn.easyjson.tests.cases.fastjson.impl;
+
+import com.jn.easyjson.tests.cases.EasyJsonPropertyNamingTest;
+
+public class FastjsonImplPropertyNamingTest extends EasyJsonPropertyNamingTest {
+
+}
