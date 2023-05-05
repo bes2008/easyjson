4cce6fd1b8313e2f92c581c25ab400d859295656
fjn1194361820 <fs1194361820@163.com>
2023-05-05 13:54:03 +0800

    * 更新测试用例

--- easyjson-test/easyjson-test-base/src/main/java/com/jn/easyjson/tests/cases/EasyjsonNestedJsonStringTests.java
+++ easyjson-test/easyjson-test-base/src/main/java/com/jn/easyjson/tests/cases/EasyjsonNestedJsonStringTests.java
@@ -5,9 +5,12 @@ import com.jn.easyjson.core.node.JsonObjectNode;
 import com.jn.easyjson.core.util.JSONs;
 import com.jn.langx.io.resource.Resource;
 import com.jn.langx.io.resource.Resources;
+import com.jn.langx.text.StringTemplates;
 import com.jn.langx.util.Strings;
 import com.jn.langx.util.function.Consumer;
 import com.jn.langx.util.io.Charsets;
+import com.jn.langx.util.struct.counter.IntegerCounter;
+import com.jn.langx.util.struct.counter.SimpleIntegerCounter;
 import org.testng.annotations.Test;
 
 
@@ -16,23 +19,27 @@ public class EasyjsonNestedJsonStringTests {
     public void test001() {
         Resource res = Resources.loadClassPathResource("00DE8E3E-95E3-4885-A760-A269A6836AE2.txt", EasyjsonNestedJsonStringTests.class);
 
+        IntegerCounter counter = new SimpleIntegerCounter(0);
         Resources.readLines(res, Charsets.UTF_8, new Consumer<String>() {
             @Override
             public void accept(String line) {
-                if (Strings.isNotBlank(line)) {
-                    JsonTreeNode node = JSONs.toJsonTreeNode(line);
-                    if (node instanceof JsonObjectNode) {
-                        JsonTreeNode message = ((JsonObjectNode) node).getProperty("message");
-                        if (message.isJsonPrimitiveNode()) {
-                            String msg = message.getAsString();
-                            System.out.println(msg);
-                            JsonTreeNode messageNode = JSONs.parse(msg);
-                            System.out.println(messageNode.isJsonObjectNode());
-                            System.out.println(messageNode);
+                if (Strings.isNotBlank(line) && counter.get()<200) {
+                    counter.increment();
+                    if(!line.contains("container-cached.log")) {
+                        System.out.println(StringTemplates.formatWithPlaceholder("************line: {}*************", counter.get()));
+                        JsonTreeNode node = JSONs.toJsonTreeNode(line);
+                        if (node instanceof JsonObjectNode) {
+                            JsonTreeNode message = ((JsonObjectNode) node).getProperty("message");
+                            if (message.isJsonPrimitiveNode()) {
+                                String msg = message.getAsString();
+                                JsonTreeNode messageNode = JSONs.parse(msg);
+                                System.out.println(StringTemplates.formatWithPlaceholder("not object: {}",messageNode));
+                            }else{
+                                System.out.println(message);
+                            }
                         }
                     }
 
-                    System.out.println("*************************");
                 }
             }
         });
